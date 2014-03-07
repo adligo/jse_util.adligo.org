afb10c5cb5a6a445de60259181977171a0f67cf0
scott <scott@adligo.com>
2014-03-07 17:56:17 -0600

    line.seperator for gWT

--- src/org/adligo/jse/util/JSEAppenderFactory.java
+++ src/org/adligo/jse/util/JSEAppenderFactory.java
@@ -1,9 +1,13 @@
 package org.adligo.jse.util;
 
 import org.adligo.i.util.shared.AppenderFactory;
-import org.adligo.i.util.shared.I_Factory;
+import org.adligo.i.util.shared.I_AppenderFactory;
 
-public class JSEAppenderFactory extends AppenderFactory implements I_Factory {
+public class JSEAppenderFactory extends AppenderFactory implements I_AppenderFactory {
+	public String getLineSeperator() {
+		return System.getProperty("line.seperator");
+	}
+	
 	public Object createNew(Object p) {
 		return new JSEAppender();
 	}
